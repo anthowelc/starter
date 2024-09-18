3db07e8394a428228ece6a8dd192329ff852f3b9
Antho Welc <welc.anthony@gmail.com>
2024-09-18 16:36:34 +0200

    simplfy content

--- src/main.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('button').addEventListener('click', () => {
-  const text = document.querySelector('p')
-  text.textContent = 'Le texte a changé !'
-  text.style.color = 'red'
-})
---
+++ src/scripts/main.js
@@ -0,0 +1,1 @@
+console.log("Si ce texte s'affiche dans la console du navigateur c'est gagné ! (F12 sur Chrome puis onglet console)")
